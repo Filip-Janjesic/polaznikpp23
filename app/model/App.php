7946dac24539934b9cf53fe5fc1de95f237fc670
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-08-25 20:25:58 +0200

    završetak controlera

--- app/model/App.php
+++ app/model/App.php
@@ -13,8 +13,28 @@ class App
         //echo '</pre>';
 
         $klasa='';
-        if(!isset($djelovi[1]) || $djelovi[1]=''){
+        if(!isset($djelovi[1]) || $djelovi[1]==''){
             $klasa='Index';
+        }else{
+            $klasa = ucfirst($djelovi[1]);
+        }
+        $klasa .='Controller';
+        //echo $klasa;
+
+        $metoda = ''; // implementacijski je to i dalje funkcion
+        if(!isset($djelovi[2]) || $djelovi[2]==''){
+            $metoda='index';
+        }else{
+            $metoda = $djelovi[2];
+        }
+        //echo $klasa . '->' . $metoda;
+        if(class_exists($klasa) && method_exists($klasa,$metoda)){
+            $instanca = new $klasa();
+            $instanca->$metoda();
+        }else{
+            //error page
+            echo 'Čak niti HGSS ne može naći što tražite ' . 
+                $klasa . '->' . $metoda;
         }
     }
 }
