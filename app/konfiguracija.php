5f7b3b145d033fdd375e9e28a9a54e44662083e3
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-09-01 21:02:57 +0200

    kraj predavanja

--- app/konfiguracija.php
+++ app/konfiguracija.php
@@ -2,5 +2,11 @@
 
 return [
     'nazivApp'=>'Edunova APP',
-    'url'=>'http://polaznikpp23.xyz/'
+    'url'=>'http://polaznikpp23.xyz/',
+    'baza'=>[
+        'server'=>'localhost',
+        'baza'=>'edunovapp23',
+        'korisnik'=>'edunova',
+        'lozinka'=>'edunova'
+    ]
 ];
