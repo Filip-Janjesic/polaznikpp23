24d7e186bbd36e4f6b71faa839d4e0ad57d701df
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-09-15 21:46:41 +0200

    predavanje 15.9

--- app/controller/SmjerController.php
+++ app/controller/SmjerController.php
@@ -5,6 +5,18 @@ class SmjerController extends AutorizacijaController
 
     private $viewDir = 'privatno' . DIRECTORY_SEPARATOR . 
                         'smjer' . DIRECTORY_SEPARATOR;
+    private $smjer;
+    private $poruka;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->smjer = new stdClass();
+        $this->smjer->naziv='';
+        $this->smjer->cijena='0,00';
+        $this->smjer->trajanje='100';
+        $this->smjer->certifikat=true;
+    }
 
     public function index()
     {
@@ -15,7 +27,111 @@ class SmjerController extends AutorizacijaController
 
     public function novi()
     {
-        $this->view->render($this->viewDir . 'novi');
+        $this->view->render($this->viewDir . 'novi',[
+            'smjer'=>$this->smjer,
+            'poruka'=>'Popunite sve podatke'
+        ]);
+    }
+
+    public function dodajnovi()
+    {
+        if(!$_POST){
+           $this->novi();
+           return;
+        }
+
+        $this->smjer = (object)$_POST;
+
+
+        if(
+            
+            $this->kontrolaNaziv() 
+        && $this->kontrolaTrajanje()
+        && $this->kontrolaCijena()
+        && $this->kontrolaCertifikat()
+        
+        ){
+            //ide spremanje u bazu
+            $this->smjer->cijena = str_replace(array('.', ','), array('', '.'), 
+            $this->smjer->cijena);
+            Smjer::create((array)($this->smjer));
+            $this->index();
+        }else{
+            $this->view->render($this->viewDir . 'novi',[
+                'smjer'=>$this->smjer,
+                'poruka'=>$this->poruka
+            ]); 
+        }
+    }
+
+    private function kontrolaNaziv()
+    {
+        if(!isset($this->smjer->naziv)){
+            $this->poruka="Naziv obavezno";
+            return false;
+        }
+        if(strlen(trim($this->smjer->naziv))===0){
+            $this->poruka="Naziv obavezno";
+            return false;
+        }
+        if(strlen(trim($this->smjer->naziv))>50){
+            $this->poruka="Naziv predugačak";
+            return false;
+        }
+        return true;
+    }
+
+    private function kontrolaTrajanje()
+    {
+        if(!isset($this->smjer->trajanje)){
+            $this->poruka="Trajanje obavezno";
+            return false;
+        }
+
+        if(strlen(trim($this->smjer->trajanje))===0){
+            $this->poruka="Trajanje obavezno";
+            return false;
+        }
+
+        $broj = (int) $this->smjer->trajanje;
+        if($broj<=0){
+            $this->poruka="Trajanje mora biti pozitivan cijeli broj";
+            return false;
+        }
+        return true;
+    }
+
+    private function kontrolaCijena()
+    {
+        if(!isset($this->smjer->cijena)){
+            $this->poruka="Cijena obavezno";
+            return false;
+        }
+
+        if(strlen(trim($this->smjer->cijena))===0){
+            $this->poruka="Cijena obavezno";
+            return false;
+        }
+
+        $broj = (float) str_replace(array('.', ','), array('', '.'), 
+                            $this->smjer->cijena);
+        //print_r($broj);
+        if($broj<=0){
+            $this->poruka="Cijena mora biti pozitivan broj";
+            return false;
+        }
+        return true;
+    }
+
+    private function kontrolaCertifikat()
+    {
+        if(!isset($this->smjer->certifikat)){
+            $this->poruka="Indikacija certifikata obavezno";
+            $this->smjer->certifikat=null;
+            return false;
+        }
+
+        return true;
     }
 
     public function promjena()
